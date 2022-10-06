ba17e73f8cee18e3bd54e931e216a3aeab46d970
Andrii <andriibiliaskyi@gmail.com>
2022-10-06 15:35:40 +0300

    print list

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,63 @@
-import React, {
-	useState,
-	// useEffect
-} from 'react';
-import './App.scss';
+import React, { useState, useEffect } from 'react';
+// import Tree from 'react-d3-tree';
+// import { Tree } from 'react-tree-graph';
+import 'react-tree-graph/dist/style.css';
 import { ListController } from './List';
+import './App.scss';
 
 function App() {
-	const [list] = useState(new ListController());
-	// const [listNodes, setListNodes] = useState(list.getListNodes());
+	const [list, setList] = useState(new ListController());
+	const [listNodes, setListNodes] = useState<any>([{}]);
+	const [i, setI] = useState(0);
 
 	// useEffect(() => {
 	// 	setListNodes(list.getListNodes());
-	// }, [list]);
+	// });
+
+	const createTree = (data) => {
+		if (data.next) {
+			return (
+				<ul>
+					<li>
+						<p>data: {data.data}</p>
+						<p>next: </p>
+						{createTree(data.next)}
+					</li>
+				</ul>
+			);
+		} else {
+			return <p>{data.data}</p>;
+		}
+	};
 
 	return (
 		<div>
-			<button
-				onClick={() => {
-					list.addNode(new Date().toISOString());
-					// setListNodes(list.getListNodes());
-				}}
-			>
-				Add Node
-			</button>
-			{/* {listNodes.map((e) => (
-				<div key={e.data}>{JSON.stringify(e)}</div>
-			))} */}
-			<button
-				onClick={() => {
-					console.log(list.getListNodes());
-				}}
-			>
-				Print
-			</button>
+			<div className='list'>
+				{/* <ul>
+					{listNodes[0].map((e) => (
+						<li key={Date.now()}>{}</li>
+					))}
+				</ul> */}
+				<ul>{createTree(listNodes[0])}</ul>
+				{/* <Tree data={listNodes[0]} /> */}
+				{/* <Tree data={listNodes[0]} height={400} width={400} />; */}
+
+				{/* {JSON.stringify(listNodes[0])} */}
+			</div>
+			<div className='buttons'>
+				<button
+					className='button'
+					onClick={() => {
+						list.addNode(new Date().toISOString());
+						setList(list);
+						setListNodes(list.getListNodes());
+						setI(i + 1);
+						console.log(listNodes);
+					}}
+				>
+					Додати елемент
+				</button>
+			</div>
 		</div>
 	);
 }
--- src/List.js
+++ src/List.js
@@ -13,45 +13,30 @@ class ListController {
 		node.data = this.listNodes.length + 1;
 		node.next = null;
 
-		// let node = { data: this.listNodes.length + 1, next: null };
-
 		let listTemp = this.listNodes;
 		if (listTemp.at(-1)) {
 			let lastNode = this.listNodes.at(-1);
 			lastNode = { ...lastNode, next: node };
 			listTemp.pop();
-			// console.log('last', lastNode);
-			// console.log('this', node);
 			listTemp.push(lastNode);
 		}
 		listTemp.push(node);
 
-		let listReversed = listTemp.reverse();
-		// console.log(listReversed.map((e) => e.data));
-		// listReversed = listReversed.map((e, i) => {
-		// 	if (i !== 0) {
-		// 		console.log(e, listReversed[i - 1]);
-		// 		return { ...e, next: listReversed[i - 1] };
-		// 	}
+		if (listTemp.length > 1) {
+			let listReversed = listTemp.reverse();
 
-		// 	return e;
-		// });
+			for (let i = 0; i < listReversed.length; i++) {
+				listReversed[i] = { ...listReversed[i], next: listReversed[i - 1] };
+			}
 
-		for (let i = 0; i < listReversed.length; i++) {
-			listReversed[i] = { ...listReversed[i], next: listReversed[i - 1] }
+			this.listNodes = listReversed.reverse();
 		}
-
-		console.log();
-		// listTemp = listTemp.map((element) => {});
-
-		this.listNodes = listReversed.reverse();
 	}
 
 	printList(n) {
 		let list = [];
 
 		while (n != null) {
-			// list.push(n.data);
 			list.push({ data: n.data, next: n.next });
 			n = n.next;
 		}
